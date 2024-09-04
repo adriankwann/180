89c4fe1679492485dc065d994c9bb826711d15fb
Adrian Kwan <104391066+adriankwann@users.noreply.github.com>
2024-09-04 11:39:04 -0700

    cfg

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enable static export mode
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // If using local images, set this to avoid image optimization issues
+  },
+};
 
 export default nextConfig;
